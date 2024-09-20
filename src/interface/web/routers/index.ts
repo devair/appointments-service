a96e207f23fd8ab3bd83d0e195ce42e682862d5e
Devair F Tonon <devairft@gmail.com>
2024-09-19 22:23:12 -0300

    add middleware to validate request authorization

--- src/interface/web/routers/index.ts
+++ src/interface/web/routers/index.ts
@@ -2,11 +2,12 @@ import { Router } from "express";
 import { DoctosApi } from "../api/DoctosApi"
 import { doctorsRouter } from "./doctors.router"
 import { DataSource } from "typeorm"
+import { authenticateToken } from "./Middleware"
 export const router = (dataSource: DataSource) => {
         
     const router = Router()
     const doctorsApi = new DoctosApi(dataSource)
-    router.use('/doctors', doctorsRouter(doctorsApi))
+    router.use('/doctors', authenticateToken, doctorsRouter(doctorsApi))
 
     return router
 }
